d90d5de845ba137cc0d78da3519651393090af83
MaryamMehzabinZahin <zahinewu@gmail.com>
2022-03-24 17:15:52 +0600

    rendering classes dynamically

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,22 +2,31 @@ import React, { Component, createContext } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 100,
+        count: 202,
         imageUrl: "https://picsum.photos/200"
     };
     render() { 
+
+        let classes = this.newMethod();
         return (
         <div>
-        <img src={this.state.imageUrl} alt=""/>
-        <span>{this.formatCount()}</span>
+        
+        <span className={classes}>{this.formatCount()}</span>
         <button>increase</button>
         </div>
         );
     }
 
+    newMethod() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "danger";
+        return classes;
+    }
+
     formatCount()
     {
-        
+        const {count} = this.state;
+        return count ===0 ? "Zero": count;
     }
 }
 
