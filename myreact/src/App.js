7e4897b3bde88d93ee9ddf45ff40391fdf65ea2a
MaryamMehzabinZahin <zahinewu@gmail.com>
2022-03-25 14:19:13 +0600

    state

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import User from './User';
 function App() {
+  const [data,setData]=useState(0);
+  function updateData()
+  {
+    setData(data+1)
+  }
+  console.warn("_____________");
   return (
     <div className="App">
       <header className="App-header">
-       <h1> in app.js</h1>
-       <User/>
+       <h1> {data}</h1>
+       <button onClick={updateData} > Update</button>
+      
       </header>
     </div>
   );
